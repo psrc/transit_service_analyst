19b3c46e66ca8955192cc8cad7d7222f68486332
stefancoe <coestefan@gmail.com>
2022-01-03 18:55:52 -0800

    Adding setup.py.

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup (
+    name="transit_service_analyst",
+    version = "0.1.0",
+    author="psrc staff",
+    author_email="scoe@psrc.org",
+    description = "Tools to support transit service analysis",
+    packages=['transit_service_analyst'],
+    python_requires=">=3.6",
+    )

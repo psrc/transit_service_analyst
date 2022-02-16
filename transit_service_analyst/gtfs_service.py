eb57c4d762c5495b1820af76aa31918568617b42
stefancoe <coestefan@gmail.com>
2022-02-16 08:12:55 -0800

    Removing start_time and end_time parameters.

--- transit_service_analyst/gtfs_service.py
+++ transit_service_analyst/gtfs_service.py
@@ -7,23 +7,21 @@ from shapely.geometry import LineString
 from .gtfs_schema import GTFS_Schema
 import time
 
+
 class Service_Utils(object):
     """
-    Documentation forthcoming.
+    Main class to store GTFS data and provde helper
+    functions.
     """
 
-    #: File with routes.
-    #: TODO document format
-
-    def __init__(self, gtfs_dir, service_date, start_time, end_time):
+    def __init__(self, gtfs_dir, service_date):
         """
-        Add docstring.
+        Instantiate class with directory of GTFS Files and a service_date for
+        which to get service.
         """
         self.gtfs_dir = gtfs_dir
         self.service_date = service_date
         self.int_service_date = int(service_date)
-        self.start_time = start_time
-        self.end_time = end_time
         self._crs_epsg = 4326
 
         # gtfs properties:
@@ -55,10 +53,16 @@ class Service_Utils(object):
             self.schedule_pattern_df.rep_trip_id.unique())
 
     def __get_calendar(self):
+        """
+        Returns calendar.txt as a DataFrame.
+        """
         calendar_df = pd.read_csv(os.path.join(self.gtfs_dir, 'calendar.txt'))
         return GTFS_Schema.Calendar.validate(calendar_df)
 
     def __get_calendar_dates(self):
+        """
+        Returns calendar_dates.txt as a DataFrame.
+        """
         if os.path.exists(os.path.join(
                 self.gtfs_dir, 'calendar_dates.txt')) is False:
             calendar_dates = pd.DataFrame(
@@ -70,6 +74,10 @@ class Service_Utils(object):
         return GTFS_Schema.Calendar_Dates.validate(calendar_dates)
 
     def __get_trips(self):
+        """
+        Gets records in trips.txt for the service_ids that represent
+        the service_date passed into the constructor. Returns a DataFrame.
+        """
         trips_df = pd.read_csv(os.path.join(self.gtfs_dir, 'trips.txt'))
         trips_df = GTFS_Schema.Trips.validate(trips_df)
         trips_df = trips_df[trips_df['service_id'].isin(self.service_ids)]
@@ -77,6 +85,10 @@ class Service_Utils(object):
         return trips_df
 
     def __get_routes(self) -> pd.DataFrame:
+        """
+        Gets records in routes.txt for the trips that represent the
+        service_date passed into the constructor. Returns a DataFrame.
+        """
         routes_df = pd.read_csv(os.path.join(self.gtfs_dir, 'routes.txt'))
         routes_df = GTFS_Schema.Routes.validate(routes_df)
         routes_df = routes_df[routes_df['route_id'].isin(
@@ -84,6 +96,10 @@ class Service_Utils(object):
         return routes_df
 
     def __get_stop_times(self):
+        """
+        Gets records in stop_times.txt for the trips that represent
+        the service_date passed into the constructor. Returns a DataFrame.
+        """
         stop_times_df = pd.read_csv(os.path.join(
             self.gtfs_dir, 'stop_times.txt'))
         stop_times_df = GTFS_Schema.Stop_Times.validate(stop_times_df)
@@ -92,6 +108,10 @@ class Service_Utils(object):
         return stop_times_df
 
     def __get_stops(self):
+        """
+        Gets records in stops.txt for the stops used by trips represented in
+        the service_date passed into the constructor. Returns a DataFrame.
+        """
         stops_gdf = pd.read_csv(os.path.join(self.gtfs_dir, 'stops.txt'))
         stops_gdf = GTFS_Schema.Stops.validate(stops_gdf)
         stops_gdf = stops_gdf[stops_gdf['stop_id'].isin(self.stop_list)]
@@ -101,6 +121,11 @@ class Service_Utils(object):
         return stops_gdf
 
     def __get_shapes(self):
+        """
+        Gets records in shapes.txt for the shape_ids in trips that represent
+        the service_date passed into the constructor. The sequence of points
+        are converted to line geometry and returned as a GeoDataFrame.
+        """
         gdf = pd.read_csv(os.path.join(self.gtfs_dir, 'shapes.txt'), dtype={
             'shape_id': str})
         gdf = GTFS_Schema.Shapes.validate(gdf)
@@ -115,8 +140,8 @@ class Service_Utils(object):
 
     def __get_service_ids(self):
         """
-        Returns a list of valid service_id(s) from each feed
-        using the user specified service_date.
+        Returns a list of valid service_id(s) from each feed using the user
+        specified service_date.
         """
         my_date = datetime(int(self.service_date[0:4]), int(
             self.service_date[4:6]), int(self.service_date[6:8]))
@@ -144,8 +169,7 @@ class Service_Utils(object):
 
     def __get_weekday(self, my_date):
         """
-        Gets the day of week from user parameter
-        service date.
+        Gets the day of week from user parameter service date.
         """
         week_days = ['monday', 'tuesday', 'wednesday',
                      'thursday', 'friday', 'saturday', 'sunday']
@@ -166,13 +190,17 @@ class Service_Utils(object):
         # grouped by tripId
         data_frame['temp'] = data_frame.groupby(group_by_col).cumcount() + 1
         # drop the old sequence column
-        data_frame = data_frame.drop(seq_col, axis=1)
+        data_frame = data_frame.drop(columns=[seq_col], axis=1)
         # rename new column:
         data_frame = data_frame.rename(columns={'temp': seq_col})
 
         return data_frame
 
     def __get_trip_list(self):
+        """
+        Returns a list of trip_ids used by the service_date passed into the
+        constructor.
+        """
         trips = self._df_all_stops_by_trips.trip_id.tolist()
         trips = list(set(trips))
         return trips
@@ -226,6 +254,9 @@ class Service_Utils(object):
         return seconds
 
     def __convert_to_seconds(self, row, field):
+        """
+        Converts from hhmmss format to seconds.
+        """
         h, m, s = row[field].split(':')
         return int(h) * 3600 + int(m) * 60 + int(s)
 
@@ -282,6 +313,12 @@ class Service_Utils(object):
         return my_dict
 
     def __get_schedule_pattern_df(self):
+        """
+        Returns a DataFrame with a field for each trip_id used
+        to represent a unique stop_pattern (rep_trip_id) and a
+        column with the other trip_ids that share the same stop
+        pattern
+        """
         rows = []
         for route_id, trips in self._schedule_pattern_dict.items():
             for trip_id, data in trips.items():
@@ -295,13 +332,13 @@ class Service_Utils(object):
                         left_on=["trip_id2"], right_on=["trip_id"])
         df2 = df2.rename(
             columns={'trip_id1': 'rep_trip_id', 'trip_id': 'orig_trip_id'})
-        df2 = df2.drop('trip_id2', 1)
+        df2 = df2.drop(columns=['trip_id2'])
         return df2
 
     def frequencies_to_trips(self):
         """
         For each trip_id in frequencies.txt, calculates the number
-        of trips and creates records for each trip in trips.txt and 
+        of trips and creates records for each trip in trips.txt and
         stop_times.txt. Deletes the original represetative trip_id
         in both of these files. 
         """
@@ -324,11 +361,11 @@ class Service_Utils(object):
         # does not include a final one that leaves the first
         # stop at end_time in frequencies. I think this is the
         # correct interpredtation of the field description:
-        # 'Time at which service changes to a different headway 
+        # 'Time at which service changes to a different headway
         # (or ceases) at the first stop in the trip.'
 
-        # Rounding total trips to make sure all trips are counted 
-        # when end time is in the following format: 14:59:59, 
+        # Rounding total trips to make sure all trips are counted
+        # when end time is in the following format: 14:59:59,
         # instead of 15:00:00.
 
         frequencies['total_trips'] = ((
@@ -366,7 +403,8 @@ class Service_Utils(object):
         stop_times_update = stop_times_update.loc[
             stop_times_update.index.repeat(
                 stop_times_update['total_trips'])].reset_index(drop=True)
-        # handles cae of repeated trip_ids
+
+        # handles case of repeated trip_ids
         stop_times_update['counter'] = stop_times_update.groupby(
             ['frequency_id', 'stop_id']).cumcount()
         stop_times_update['departure_time_secs'] = stop_times_update[
@@ -404,6 +442,12 @@ class Service_Utils(object):
         return trips, stop_times
 
     def get_tph_by_line(self):
+        """
+        Returns a DataFrame with records for each rep_trip_id and
+        columns with the number of trips for each hour after midnight
+        with service. For example 2:00-3:00 AM is called hour_2 and
+        3:00-4:00 PM is called hour_15.
+        """
         # get the first stop for every trip
         first_departure = self._df_all_stops_by_trips.sort_values(
             'stop_sequence', ascending=True).groupby(
@@ -430,10 +474,17 @@ class Service_Utils(object):
         t = t.merge(self.trips[['route_id', 'trip_id',
                     'direction_id']], how='left', left_on='rep_trip_id',
                     right_on='trip_id')
+        t.drop(columns=['trip_id'], axis=1, inplace=True)
 
         return t
 
     def get_tph_at_stops(self):
+        """
+        Returns a DataFrame with records for each stop_id and
+        columns with the number of trips for each hour after midnight
+        with service. For example 2:00-3:00 AM is called hour_2 and
+        3:00-4:00 PM is called hour_15.
+        """
         df = self._df_all_stops_by_trips.groupby(
             ['stop_id', 'departure_time_hrs'])['departure_time_hrs'].count()
 
@@ -451,15 +502,25 @@ class Service_Utils(object):
         return t
 
     def get_lines_gdf(self):
+        """
+        Returns a GeoDataFrame with records for each rep_trip_id and
+        line geomery for the shape_id used by the trip_id. Useful GTFS
+        columns inlcude route_id, direction_id, route_type, route_short_name,
+        route_long_name, and route_desc.
+        """
         rep_trips = self.trips[self.trips['trip_id'].isin(
             self.__rep_trip_list)]
         rep_trips = rep_trips.merge(self.routes, how='left', on='route_id')
         rep_trips = self.shapes.merge(rep_trips, how='right', on='shape_id')
         rep_trips.rename(columns={'trip_id': 'rep_trip_id'}, inplace=True)
-        assert rep_trips.geometry.hasnans == False
+        assert rep_trips.geometry.hasnans==False
         return rep_trips
 
     def get_line_stops_gdf(self):
+        """
+        Returns a GeoDataFrame with records for each stop for each
+        rep_trip_id.
+        """
         route_stops = self._df_all_stops_by_trips[
             self._df_all_stops_by_trips['trip_id'].isin(self.__rep_trip_list)]
         route_stops = route_stops.merge(self.stops, how='left', on='stop_id')
@@ -473,6 +534,10 @@ class Service_Utils(object):
         return route_stops
 
     def get_line_time(self):
+        """
+        Returns a DataFrame with records for each rep_trip_id
+        and their total service time.
+        """
         first = self._df_all_stops_by_trips.groupby(
             ['trip_id'])['departure_time_mins'].first()
         first.rename('first', inplace=True)
@@ -489,15 +554,26 @@ class Service_Utils(object):
         return df
 
     def get_service_hours_by_line(self):
+        """
+        Returns a DataFrame with records for each rep_trip_id and columns with
+        the number of service hours for each hour after midnight with service.
+        For example 2:00-3:00 AM is called hour_2 and 3:00-4:00 PM is called
+        hour_15.
+        """
         df = self.get_line_time()
         df = df.groupby('rep_trip_id')['total_line_time'].sum().to_frame()
         df.reset_index(inplace=True)
         df = df.merge(self.trips[['route_id', 'trip_id', 'direction_id']],
                       how='left', left_on='rep_trip_id', right_on='trip_id')
 
-        return df[['rep_trip_id', 'total_line_time', 'route_id', 'direction_id']]
+        return df[['rep_trip_id', 'total_line_time', 'route_id',
+                  'direction_id']]
 
     def get_routes_by_stops(self):
+        """
+        Returns a DataFrame with records for each rep_trip_id and a column
+        holding a list of stops for each line.
+        """
         df = self._df_all_stops_by_trips[self._df_all_stops_by_trips[
             'trip_id'] == self._df_all_stops_by_trips['rep_trip_id']]
         df = pd.DataFrame(df.groupby('stop_id')['route_id'].apply(
@@ -506,6 +582,10 @@ class Service_Utils(object):
         return df
 
     def get_total_trips_by_line(self):
+        """
+        Returns a DataFrame with records for each rep_trip_id and a column
+        holding the total number of trips for each line.
+        """
         df = self.schedule_pattern_df.groupby('rep_trip_id').count(
                  ).reset_index()[['rep_trip_id', 'orig_trip_id']]
         df = df.rename(columns={'orig_trip_id': 'total_trips'})
--- transit_service_analyst/load_gtfs.py
+++ transit_service_analyst/load_gtfs.py
@@ -1,5 +1,6 @@
 from .gtfs_service import Service_Utils
 
+
 def load_gtfs(gtfs_dir, service_date, start_time, end_time):
-    gtfs_service = Service_Utils(gtfs_dir, service_date, start_time, end_time)
+    gtfs_service = Service_Utils(gtfs_dir, service_date)
     return gtfs_service
